f27f28aa8e9a68960cfc627873ae037aa79aef08
kjleroy <kjleroy2007@gmail.com>
2017-06-27 19:03:12 -0400

    trying to figure it out

---
+++ app/assets/scripts/App.js
@@ -0,0 +1,7 @@
+var Person = require('./modules/Person.js');
+
+var john = new Person("Johnny Appleseed", "Orange");
+john.greet();
+
+var jane = new Person("Janice Patrice", "Green");
+jane.greet();
